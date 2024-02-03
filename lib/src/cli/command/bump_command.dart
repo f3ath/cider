758ed2e2a531e868abbb913204639560e386624a
Alexey <f3ath@users.noreply.github.com>
2024-02-03 15:32:13 -0800

    Release (#72)

--- lib/src/cli/command/bump_command.dart
+++ lib/src/cli/command/bump_command.dart
@@ -4,21 +4,6 @@ import 'package:cider/src/cli/command/bump_sub_command.dart';
 import 'package:cider/src/cli/command/cider_command.dart';
 import 'package:version_manipulation/mutations.dart';
 
-enum BumpType {
-  breaking(BumpBreaking(), 'Bump the breaking version'),
-  major(BumpMajor(), 'Bump the major version'),
-  minor(BumpMinor(), 'Bump the minor version'),
-  patch(BumpPatch(), 'Bump the patch version'),
-  build(BumpBuild(), 'Bump the build version'),
-  pre(BumpPreRelease(), 'Bump the pre-release version'),
-  release(Release(), 'Bump the release version');
-
-  const BumpType(this.mutation, this.description);
-
-  final VersionMutation mutation;
-  final String description;
-}
-
 class BumpCommand extends CiderCommand {
   BumpCommand(super.console) {
     for (final type in BumpType.values) {
@@ -38,4 +23,19 @@ class BumpCommand extends CiderCommand {
     throw UsageException(
         'Bump command can only be used with subcommands', usage);
   }
+}
+
+enum BumpType {
+  breaking(BumpBreaking(), 'Bump the breaking version'),
+  major(BumpMajor(), 'Bump the major version'),
+  minor(BumpMinor(), 'Bump the minor version'),
+  patch(BumpPatch(), 'Bump the patch version'),
+  build(BumpBuild(), 'Bump the build version'),
+  pre(BumpPreRelease(), 'Bump the pre-release version'),
+  release(Release(), 'Bump the release version');
+
+  const BumpType(this.mutation, this.description);
+
+  final VersionMutation mutation;
+  final String description;
 }
